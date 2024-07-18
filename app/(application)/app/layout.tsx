094771c9c4a1fea1851b30aeb667ead1f838c2c5
Lucas <lucasdsveras@gmail.com>
2024-07-17 22:22:57 -0300

    Change sans serif font to Inter

--- app/(application)/app/layout.tsx
+++ app/(application)/app/layout.tsx
@@ -2,13 +2,25 @@ import React from "react";
 import "@/styles/globals.css";
 import AppHeader from "@/components/app-header";
 import AppNavigation from "@/components/app-navigation";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default async function ApplicationLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <div className="grid min-h-screen w-full grid-cols-[224px_1fr]">
           <AppNavigation />
 
--- app/(homepage)/layout.tsx
+++ app/(homepage)/layout.tsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "@/styles/globals.css";
 import Header from "@/components/header";
+import { Inter as FontSans } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html>
-      <body>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
         <Header />
         <main>{children}</main>
       </body>
