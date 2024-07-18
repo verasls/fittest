0ad5afc8b92e4c611a3c3c76b1ab2356b28cd76c
Lucas <lucasdsveras@gmail.com>
2024-07-17 22:34:15 -0300

    Add buttons to create new client and new evaluation

--- app/(application)/app/dashboard/page.tsx
+++ app/(application)/app/dashboard/page.tsx
@@ -1,10 +1,25 @@
+import { Button } from "@/components/ui/button";
 import { Heading } from "@/components/ui/heading";
 import { auth } from "@/lib/auth";
+import { Ruler, Users } from "lucide-react";
 
 export default async function Page() {
   const session = await auth();
 
   return (
-    <Heading type="h2">Olá, {session!.user!.name!.split(" ").at(0)}!</Heading>
+    <div>
+      <Heading type="h2">Olá, {session!.user!.name!.split(" ").at(0)}!</Heading>
+
+      <div className="flex gap-3 pt-6">
+        <Button className="h-9">
+          <Users className="mr-2 h-4 w-4" />
+          <span>Novo cliente</span>
+        </Button>
+        <Button className="h-9">
+          <Ruler className="mr-2 h-4 w-4" />
+          <span>Nova avaliação</span>
+        </Button>
+      </div>
+    </div>
   );
 }
